d43e68f076fc91806378c3de93978d98cbbb85c6
Chad A. Woitas <woitas.chad@gmail.com>
2018-05-18 17:34:54 -0600

    Uploading All The Pages
    TODO listed on app.js

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+/*
+ Project: PBR Test Site
+ Author: Chad A. Woitas
+ Repo: githhub.com/satiowadahc/PBRprototyping
+
+ Current TODO List:
+	- Photo Galleries - nanogallery
+	- Fix Nav - CSS 
+	- Add content to Pages
+		About - Copy Pasta?
+		Automotive
+		Live Auctions
+		Upcoming Auctions
+		Buyer
+		Sellers
+		Store
+		Contact
+	- Member list
+		- DB?
+	- Encryption - Cert Bot 
+*/
+
+
  // load data
 const express = require('express');
 const app = express();
@@ -15,9 +38,6 @@ app.use((req,res,next)=>{
 	next();
 });
 
-
-
-
  // Pages
  
  // Home
@@ -25,8 +45,33 @@ app.get('/', (req,res)=>{
 	res.render('index');
 });
 
+app.get('/about', (req,res)=>{
+	res.render('about');
+});
 
+app.get('/automotive', (req,res)=>{
+	res.render('automotive');
+});
 
+app.get('/liveauctions', (req,res)=>{
+	res.render('liveauction');
+});
+
+app.get('/upcoming', (req,res)=>{
+	res.render('upcoming');
+});
+
+app.get('/buyer', (req,res)=>{
+	res.render('buyers');
+});
+
+app.get('/store', (req,res)=>{
+	res.render('store');
+});
+
+app.get('/contact', (req,res)=>{
+	res.render('contact');
+});
 
  // Start the server
 app.listen(8020, ()=>{
